59c00124da33a2b1ca8e7a2a9b8baa075772cb71
Тюленев Артём <92201659+RuNforest90@users.noreply.github.com>
2021-12-17 16:36:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-var catNames = ['Pushok', 'Marsik', 'Mafik', 'Tusik'];
-var catColors = ['blue', 'white', 'red',];
-var catlen = [89, 56, 13,];
-var cat = {
-	name: catNames[random(0, catNames.length - 1)],
-	color: 'orange',
-	len: 30,
-};
-var cat={
-	name: catNames[random(0, catNames.length - 1)],
-	color: catColors[random(0, catColors.length - 1)],
-	len: catlen[random(0, catlen,length - 1)],
+var catNames = ['Petrov', 'Valera', 'Mafin', 'Bob'];
+
+var arrayCats = [];
+
+
+for (var i = 0; i < random(1, 12); i++){
+	var cat = {
+	    name: catNames[random(0, catNames.length - 1)],
+	    color: 'orange',
+	    len: 67,
+    };
+    arrayCats.push(cat);
 }
 
-var arrayCats = [cat, cat, cat];
- 
-for (var i = 0; i < arrayCats.length; i++) {
+for (var i = 0; i < random(1, 12); i++){
+	var cat = {
+	    name: catNames[random(0, catNames.length - 1)],
+	    color: 'white',
+	    len: 45,
+	};
+	arrayCats.push(cat);
+}
+
+for (var i = 0; i < random(1, 12); i++){
+	var cat = {
+	    name: catNames[random(0, catNames.length - 1)],
+	    color: 'grey',
+	    len: 56,
+	};
+	arrayCats.push(cat);
+}
+
+for (var i = 0; i < random(1, 12); i++){
+	var cat = {
+	    name: catNames[random(0, catNames.length - 1)],
+	    color: 'red',
+	    len: 20,
+	};
+	arrayCats.push(cat);
+}
+
+for (var i = 0; i < random(1, 12); i++){
 	console.log(arrayCats[i]);
 }
- 
-function random(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
-console.log(random(0, 5));
+function random(min, max) {
+	return Math.floor(Math.random() * (max - min * 1)) + min;
+}
