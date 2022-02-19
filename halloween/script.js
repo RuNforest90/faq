af633ac1be0d82809c6a366dbefcb07e15ea78c6
Тюленев Артём <92201659+RuNforest90@users.noreply.github.com>
2022-02-19 11:42:35 +0500

    Add files via upload

--- halloween/script.js
+++ halloween/script.js
@@ -1,1 +1,8 @@
-console.log('script');
+$(document).ready(function() {
+   console.log('ready');
+
+  $('#mark-1').click(function() {
+      $('#slide_1').show();
+      $('#slide_2').hide();
+  }
+});
