87b6cb581b51ca9094f497eab936c05c38c174ea
Тюленев Артём <92201659+RuNforest90@users.noreply.github.com>
2022-04-09 11:21:09 +0500

    Add files via upload

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,48 @@
- $(document).ready(function () {
-     $(".vid").text(0);
- });
+let calc = {
+    numbA: '',
+    act: null,
+    numbB: '',
+}
+
+$(document).ready(function () {
+    console.log(calc);
+     $(".vvd").text(0);
+
+     $("#1").click(function() {
+         console.log("#1");
+
+         if (calc.act === null) {
+            calc.numbA = 1;
+         } else {
+             calc.numbB = 1;
+         }
+
+         console.log(calc);
+     });
+
+     $("#plus").click(function() {
+         console.log("#plus");
+         
+         calc.act = 'plus';
+
+         console.log(calc);
+     });
+
+     $("#rvn").click(function() {
+        console.log("#rvn");
+        let rvn = null;
+        if (calc.act === "plus") {
+            rvn = Number(calc.numbA) + Number(calc.numbB);
+        }
+        if (calc.act === "mine") {
+            rvn = Number(calc.numbA) - Number(calc.numbB);
+        }
+        if (calc.act === "divi") {
+            rvn = Number(calc.numbA) / Number(calc.numbB);
+        }
+        if (calc.act === "mult") {
+            rvn = Number(calc.numbA) * Number(calc.nunbB)
+        }
+        $(".vvd").text(rvn);
+    });
+  });
